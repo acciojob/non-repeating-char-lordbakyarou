6cedcc1b86bcea1f19b477ffde2af5e5cbc61e3c
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-09 01:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++) {
+  let s = str[i];
+  let arr = str.match(new RegExp(s, "g"));
+
+  if (arr.length == 1) {
+    return str[i];
+  }
+}
+
+return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
